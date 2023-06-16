0700400d6bc8d5fe2e7bb5d35255abf4ddb97cb1
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-06-16 18:25:11 +0530

    code add

---
+++ HttpModule/index.js
@@ -0,0 +1,17 @@
+const http = require("http")
+
+const Server = http.createServer ( (req,res) => {
+    if(req.url == '/') {
+        res.write('<h1>Hello , Node.js Server <h1>');
+    }
+    res.end() ;
+})
+
+
+Server.listen(5000);
+
+console.log("The http server is running port no. 5000");
+
+
+
+
